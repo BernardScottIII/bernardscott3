ccf9fd5960f5b1b5c45e9925fbc455d710f53ab4
BernardScottIII <106618915+BernardScottIII@users.noreply.github.com>
2023-06-22 13:52:51 -0400

    Initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to Bernard Scott's website using SvelteKit!</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
