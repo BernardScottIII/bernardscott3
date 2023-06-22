4aa3ea45cc565b57d9cd5cce2af36444147b07f7
BernardScottIII <106618915+BernardScottIII@users.noreply.github.com>
2023-06-22 14:01:09 -0400

    second commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
 <h1>Welcome to Bernard Scott's website using SvelteKit!</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h2>Here's my first change!</h2>
+<p>Will I ever truly learn to use git properly?</p>
