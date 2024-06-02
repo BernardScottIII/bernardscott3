9509c2efda0e6b5a272449ab7c6790c0ea23b718
BernardScottIII <106618915+BernardScottIII@users.noreply.github.com>
2024-06-02 15:48:38 -0400

    Cosmetic changes

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,6 +1,8 @@
-<h1 class="container-fluid">Universal Footer</h1>
-<p><a href="/portfolio">Portfolio</a></p>
-<p><a href="/resume">Resume</a></p>
-<p><a href="/about-me">About Me</a></p>
-<p><a href="/blog">Blog</a></p>
-<p><a href="/contact-me">Contact Me</a></p>
+<div id="footer">
+    <h1 class="container-fluid">Universal Footer</h1>
+    <p><a href="/portfolio">Portfolio</a></p>
+    <p><a href="/resume">Resume</a></p>
+    <p><a href="/about-me">About Me</a></p>
+    <p><a href="/blog">Blog</a></p>
+    <p><a href="/contact-me">Contact Me</a></p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,9 @@
 <main>
-    <h1 id="app-header" class="container-fluid">Hello World!</h1>
-    <p class="container-fluid">I'm Bernard Scott, an aspiring software developer currently pursuing their Bachelor of Science in Computer Science at the University of Scranton.</p>
+    <section class="container-fluid">
+        <h1>Hello World!</h1>
+        <p>I'm Bernard Scott, an aspiring software developer currently pursuing their Bachelor of Science in Computer Science at the University of Scranton.</p>
+    </section>
+    <!-- <section class="container-fluid">
+        <h2>About Me</h2>
+    </section> -->
 </main>
